93141cacf562c5312f19bbaca692a5482ed48aca
Amir Muminov <amir_9999@mail.ru>
2020-10-11 03:29:57 +0600

    feat: added crossorigin

--- src/main/java/kz/muminov/reservationservice/controller/ReservationController.java
+++ src/main/java/kz/muminov/reservationservice/controller/ReservationController.java
@@ -6,15 +6,13 @@ import kz.muminov.reservationservice.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ReservationController {
 
     private final ReservationService reservationService;
--- src/main/java/kz/muminov/reservationservice/controller/TableController.java
+++ src/main/java/kz/muminov/reservationservice/controller/TableController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class TableController {
 
     private final TableService tableService;
